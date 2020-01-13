7b931df73769c153e7cf89f4fb3df2c59495871d
Michael P <michael.perret143@gmail.com>
2020-01-13 16:35:59 +0100

    Creation of Pioneer3DX comp.

---
+++ ComponentWebotsPioneer3DX/smartsoft/src-gen/MixedPortTargetConfigs.cmake
@@ -0,0 +1,17 @@
+# target configurations for ComponentWebotsPioneer3DXROSExtension
+IF(EXISTS ${ROS_DIR})
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} ${ROS_LIBS})
+ENDIF(EXISTS ${ROS_DIR})
+
+# target configurations for OpcUaBackendComponentGeneratorExtension
+IF(SeRoNetSDK_FOUND)
+#TARGET_LINK_LIBRARIES(${PROJECT_NAME} SeRoNetSDK::SeRoNetSDK)
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} CommBasicObjectsOpcUa)
+ENDIF(SeRoNetSDK_FOUND)
+
+# target configurations for PlainOpcUaComponentWebotsPioneer3DXExtension
+IF(Open62541CppWrapper_FOUND)
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} Open62541CppWrapper)
+TARGET_COMPILE_DEFINITIONS(${PROJECT_NAME} PUBLIC HAS_OPCUA)
+ENDIF(Open62541CppWrapper_FOUND)
+
