93b94b55991a264c8d4361207e93ac5affcb9e68
DavidMansolino <David.Mansolino@cyberbotics.com>
2020-01-16 09:53:35 +0100

    Remove unwanted files.

--- SystemWebotsRobotino3Navigation/smartsoft/build/CMakeFiles/SystemWebotsRobotino3Navigation.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SystemWebotsRobotino3Navigation.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- SystemWebotsRobotino3Navigation/smartsoft/src-gen/system/BuildExternalComponents.cmake
+++
@@ -1,5 +0,0 @@
-CMAKE_MINIMUM_REQUIRED(VERSION 3.5)
-PROJECT(SystemWebotsRobotino3Navigation)
-
-# this is a pseudo-target just to trigger CMake builder
-ADD_CUSTOM_TARGET(${PROJECT_NAME} ALL)
